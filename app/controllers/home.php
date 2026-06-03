538b0303a63737683d5bd28eb80bf289c142dc59
unknown <ngocquan1082005>
2026-06-03 11:49:11 +0700

    feat<sinhvien>: create SinhVien

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,7 +1,8 @@
 <?php
 class home{
     public function index(){
-        require_once '../app/views/home/index.php';
+        //require_once '../app/views/home/index.php';
+        require_once '../app/views/layout/masterlayout.php';
     }
     public function login(){
         require_once '../app/views/home/login.php';
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -7,6 +7,7 @@ class Controller {
 
     public function view($viewName, $data = []) {
         extract($data);
-        require_once '../app/views/' . $viewName . '.php';
+        $viewname = $viewName;
+        require_once '../app/views/layout/masterlayout.php';
     }
 }
